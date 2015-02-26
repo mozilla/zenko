e49d13892bd0c020f7f9d6726214a2cf673196ab
Daniel Thornton <dthornton@mozilla.com>
2015-02-26 13:16:05 -0800

    convert zenko to a python app

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zenko",
+    version="0.1.0",
+    description="Reports Generating Webapp for Sponsored Tiles",
+    author="Mozilla",
+    packages=find_packages(),
+    include_package_data=True,
+)
