8f81a32eacdae09f785064aa91431af328eddf13
Daniel Thornton <dthornton@mozilla.com>
2015-03-02 15:31:22 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zenko",
-    version="0.1.3",
+    version="0.1.4",
     description="Reports Generating Webapp for Sponsored Tiles",
     author="Mozilla",
     packages=find_packages(),
